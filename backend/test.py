a4aed7b2747c0c63d8755a16ca1107ed9e1991f9
Nathan Diekema <ndiekema@calpoly.edu>
2021-05-10 09:06:41 -0700

    Changed test names

--- backend/test.py
+++ backend/test.py
@@ -3,8 +3,8 @@ import backend as b
 
 # to run on Windows use command 'py -m pytest test.py'
 
-def test_1():
+def test_password_1():
     assert b.validate_password(b'test') == 0
 
-def test_2():
+def test_password_2():
     assert b.validate_password(b'wrong') == 1
