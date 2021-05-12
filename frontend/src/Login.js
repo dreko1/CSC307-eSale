aa50dd11ca7e36df474da81061cbd6c785d0b7f7
David <dreko@calpoly.edu>
2021-05-11 19:52:08 -0700

    basic login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,9 +4,26 @@ import './App.css';
 
 function Login() {
   return (
-    <div className="Login">
-    	Login
-    </div>
+<html>   
+<head>  
+<title> Login Page </title> 
+</head>    
+<body>    
+    <center> <h1> eSale Login </h1> </center>   
+    <form>  
+        <div class="container">   
+            <label>Username : </label>
+			<input type="text" placeholder="Enter Username" name="username" required></input>
+			<br></br>
+            <label>Password : </label>   
+			<input type="text" placeholder="Enter Password" name="password" required></input>
+			<br></br>
+            <button type="submit">Login</button>  
+			<br></br>
+        </div>   
+    </form>     
+</body>     
+</html>    
   );
 }
 
