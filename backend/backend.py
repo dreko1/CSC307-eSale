6dc4add1876f192ac4869b5317b741fef2a96403
Nathan Diekema <ndiekema@calpoly.edu>
2021-05-18 19:42:00 -0700

    added a preliminary form for user registration

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,9 @@
+from wtforms.fields.core import BooleanField
 import bcrypt as b
 from bson.objectid import ObjectId
 from model import Model
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, flash, render_template, jsonify, redirect, url_for
+from wtforms import Form, StringField, PasswordField, validators  # Pip install flask-wtf
 import json
 # for linking frontend-backend
 from flask_cors import CORS
@@ -15,12 +15,21 @@ CORS(app)
 
 database = Model()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>Page Not Found.</p>", 404
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
 # this method will need to be called by the method the queries the database
 def validate_password(pw, db_pw, db_salt):
     hashed_password = b.hashpw(pw, db_salt)
     return b.checkpw(hashed_password, db_pw)
 
-@app.route('/login', methods=['Post'])
+@app.route('/login', methods=['POST'])
 def login():
     requestData = request.get_json()
     username = requestData['username']
@@ -33,23 +42,55 @@ def login():
     return {}
 
 
-@app.route('/newuser', methods=['Post'])
+# Form for easier registration handling
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=20)])
+    email = StringField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [validators.Required(),
+                                          validators.EqualTo('confirm', message='Passwords must match.')])
+    confirm = PasswordField('Repeat Password')
+
+    # Terms of Service (if we even want to include it)
+    # accept_tos = BooleanField('I accept the <a href="/tos/">Terms of Service</a>' [validators.Required()])
+
+
+@app.route('/register', methods=['POST'])
 def new_user():
-    username = request.args.get('username')
-    password = request.args.get('password')
-    if username and password:
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
         salt = b.gensalt()
-        password = b.hashpw(password, salt)
+        password = b.hashpw(form.password.data, salt)
         user = database.get_user(username)
-        if not user:
-            database.create_user(username, password, salt)
+        if user:
+            flash('This Username already exists!')
+            return form
+        else:
+            database.create_user(username, email, password, salt)
             user = database.get_user(username)
             if user:
                 return user
     return {}
 
 
-@app.route('/post', methods=['Post'])
+# @app.route('/newuser', methods=['Post'])
+# def new_user():  
+    # username = request.args.get('username')
+    # password = request.args.get('password')
+    # if username and password:
+    #     salt = b.gensalt()
+    #     password = b.hashpw(password, salt)
+    #     user = database.get_user(username)
+    #     if not user:
+    #         database.create_user(username, password, salt)
+    #         user = database.get_user(username)
+    #         if user:
+    #             return user
+    # return {}
+
+
+@app.route('/post', methods=['POST'])
 def post_listing():
     requestData = request.get_json()
     user = requestData['username']
@@ -57,3 +98,11 @@ def post_listing():
     contact = requestData['contact']
     listing = database.create_listing(user, text, contact, {"image": None})
     return {}
+
+@app.route('/profile', methods=['GET', 'POST'])
+def get_information():
+    if request.method == 'GET': # Get users likes
+        # flask.redirect ?
+        requestData = request.get_json()
+    elif request.method == 'POST':
+        pass
