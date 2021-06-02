dc3c1076a50e83dc7f1229ab05ff949b8526c6d6
Chris Middleton <middleton.chris22@gmail.com>
2021-06-01 19:09:08 -0700

    display listings in frontend

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -6,60 +6,106 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import img from './dreko.jpg';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 
 const useStyles = makeStyles((theme) => ({
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
   root: {
-    maxWidth: 300
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    paddingTop: 5,
   },
-  paper: {
-    padding: theme.spacing(15),
-    textAlign: "center",
-    color: theme.palette.text.secondary
-  }
+  gridList: {
+    width: "100vw",
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
 }));
-
-
 
 function Content(props) {
   const classes = useStyles();
   
-  function buildPost(postData){
-    //look at the console to see that this function is called for each listing recieved from the server.
-    console.log(postData)
-    
-    return(
-      <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Test"
-          height="140"
-          image={img}
-          title="Test"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Test Listing
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Description
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      </Card>
-    )
-  }
-  
+  console.log(props.listings);
   return (
-    <main className={classes.content}>
-    <Toolbar />
-    {props.listings.map(buildPost)}
-    </main>
+    <div className={classes.root}>
+      <Toolbar />
+      <GridList cellHeight={240} className={classes.gridList} cols={3}>
+        {props.listings.map((listing) => (
+          <GridListTile key={listing.title}>
+          <img src={listing.image} alt={listing.title} />
+          <GridListTileBar
+            title={listing.title}
+            subtitle={<span>seller: {listing.username}</span>}
+            actionIcon={
+              <IconButton aria-label={`info about ${listing.title}`} className={classes.icon}>
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
   );
 }
+
+
 export default Content;
+
+
+////////////////////////////////////////////
+/*
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import tileData from './tileData';
+
+
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ 
+export default function TitlebarGridList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component="div">December</ListSubheader>
+        </GridListTile>
+        {tileData.map((tile) => (
+          
+        ))}
+      </GridList>
+    </div>
+  );
+}
+*/
