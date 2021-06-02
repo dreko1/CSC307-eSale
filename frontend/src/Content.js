73f39f001ff7e9083c85da3aac915f44d22bc701
David <dreko@calpoly.edu>
2021-06-01 21:23:53 -0700

    added a popup

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import Popover from '@material-ui/core/Popover';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,11 +24,27 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
   },
+  typography: {
+    padding: theme.spacing(2),
+  },
 }));
 
 function Content(props) {
   const classes = useStyles();
   
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   console.log(props.listings);
   return (
     <div className={classes.root}>
@@ -45,7 +57,20 @@ function Content(props) {
             title={listing.title}
             subtitle={<span>seller: {listing.username}</span>}
             actionIcon={
-              <IconButton aria-label={`info about ${listing.title}`} className={classes.icon}>
+              <IconButton aria-label={`info about ${listing.title}`} className={classes.icon} onClick={handleClick}>
+              <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+              >
+              <Typography className={classes.typography}>Title: {listing.title}</Typography>
+              <Typography className={classes.typography}>Description: {listing.description}</Typography>
+              <Typography className={classes.typography}>Seller: {listing.username}</Typography>
+              <Typography className={classes.typography}>Contact: {listing.contact}</Typography>
+              <Typography className={classes.typography}>Price: ${listing.price}</Typography>
+              <Typography className={classes.typography}>Zip Code: {listing.location.zip}</Typography>
+              </Popover>
                 <InfoIcon />
               </IconButton>
             }
@@ -59,53 +84,3 @@ function Content(props) {
 
 
 export default Content;
-
-
-////////////////////////////////////////////
-/*
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import tileData from './tileData';
-
-
-
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- 
-export default function TitlebarGridList() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={180} className={classes.gridList}>
-        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">December</ListSubheader>
-        </GridListTile>
-        {tileData.map((tile) => (
-          
-        ))}
-      </GridList>
-    </div>
-  );
-}
-*/
